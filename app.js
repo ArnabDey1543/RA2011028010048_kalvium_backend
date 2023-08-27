8982aea1fff8ea920c0e27ec646eeaa67be5ebeb
ArnabDey1543 <arnabdey1543@gmail.com>
2023-08-27 16:24:15 +0530

    added documentation

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const http = require('http');
 const url = require('url');
 const fs = require('fs');
-// const { writeFileSync, readFileSync } = require("fs");
 const process = require('process');
 
 const port = 3000;
@@ -9,7 +8,10 @@ const maxHistorySize = 20;
 const historyFilePath = './history.json'; // File to store history data
 const history = [];
 
-// Load history data from the file at server start
+
+
+// HISTORY
+// Loading history data from the file at server start
 try {
     const historyData = fs.readFileSync(historyFilePath);
     if (historyData) {
@@ -19,17 +21,27 @@ try {
     console.error('Error reading history file:', error);
 }
 
-const server = http.createServer(function(req, res) {
+
+
+
+// MAIN SERVER
+
+const server = http.createServer(function (req, res) {
+
+    // Getting the parsed URL
     const parsedUrl = url.parse(req.url, true);
     const pathSegments = parsedUrl.pathname.split('/');
 
+
     if (pathSegments[1] === 'history') {
-        // Handle the /history endpoint
+        // Handling the /history endpoint
         res.writeHead(200, { 'Content-Type': 'text/html' });
         const historyList = history.map(entry => `Operation: ${entry.operation}, Result: ${entry.result}`).join('<br>');
-        res.write(`Last 20 operations:<br>${historyList}`);
+        res.write(`Last 20 operations:${historyList}`);
         res.end();
     } else if (pathSegments.length >= 4 && pathSegments.length % 2 === 0) {
+
+        // creating a result string type which will append all the operand and operator
         let result = "";
         result += parseFloat(pathSegments[1]);
 
@@ -37,6 +49,7 @@ const server = http.createServer(function(req, res) {
             const operation = pathSegments[i].toLowerCase();
             const num = parseFloat(pathSegments[i + 1]);
 
+            // switch cases for all the operators
             switch (operation) {
                 case 'plus':
                     result += `+` + `${num}`;
@@ -57,6 +70,8 @@ const server = http.createServer(function(req, res) {
                         return;
                     }
                     break;
+
+                // if above switch case are not there, the default value will shown invalid
                 default:
                     res.writeHead(400, { 'Content-Type': 'text/html' });
                     res.write('Invalid operation');
@@ -65,14 +80,16 @@ const server = http.createServer(function(req, res) {
             }
         }
 
+        // evaluating the result mathematical expression and using eval() function to solve the expression using BODMAS rule.
         const evaluate = eval(result);
 
-        // Add the operation and result to the history
+        // Adding the operation and result to the history
         history.unshift({ operation: result, result: evaluate });
         if (history.length > maxHistorySize) {
             history.pop();
         }
 
+        // printing question and result
         res.writeHead(200, { 'Content-Type': 'text/html' });
         res.write(`Question: ${result}   Result: ${evaluate}`);
         res.end();
@@ -83,14 +100,11 @@ const server = http.createServer(function(req, res) {
     }
 });
 
-// Before the server process exits, save the history data to the file
-// process.on('exit', () => {
-//     try {
-//         fs.writeFileSync(historyFilePath, JSON.stringify(history), 'utf8');
-//     } catch (error) {
-//         console.error('Error writing history file:', error);
-//     }
-// });
+
+
+
+
+// Listening for SIGINT and SIGTERM signals to save history before exiting
 process.on('SIGINT', () => {
     console.log('Received SIGINT signal. Saving history and exiting...');
     saveHistoryAndExit();
@@ -112,287 +126,13 @@ function saveHistoryAndExit() {
 }
 
 
-server.listen(port, function(error) {
+
+// Listening the server 
+server.listen(port, function (error) {
     if (error) {
         console.log('Something went wrong', error);
     } else {
         console.log('Server is listening on port ' + port);
     }
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const http = require('http');
-// const url = require('url');
-// const fs = require('fs');
-
-
-// const port = 3000;
-// const maxHistorySize = 20;
-// const historyFilePath = "./history.json";
-
-// const history = [];
-
-
-
-// try {
-//     const historyData = fs.readFileSync(historyFilePath, 'utf8');
-//     if (historyData) {
-//         history.push(...JSON.parse(historyData));
-//     }
-// } catch (error) {
-//     console.error('Error reading history file:', error);
-// }
-
-
-
-
-
-
-
-// const server = http.createServer(function(req, res) {
-//     const parsedUrl = url.parse(req.url, true);
-//     const pathSegments = parsedUrl.pathname.split('/');
-
-//     if (pathSegments[1] === 'history') {
-//         // Handle the /history endpoint
-//         res.writeHead(200, { 'Content-Type': 'text/html' });
-//         const historyList = history.map(entry => `Operation: ${entry.operation}, Result: ${entry.result}`).join('<br>');
-//         res.write(`Last 20 operations:<br>${historyList}`);
-//         res.end();
-//     } else if (pathSegments.length >= 4 && pathSegments.length % 2 === 0) {
-//         let result = "";
-//         result += parseFloat(pathSegments[1]);
-
-//         for (let i = 2; i < pathSegments.length; i += 2) {
-//             const operation = pathSegments[i].toLowerCase();
-//             const num = parseFloat(pathSegments[i + 1]);
-
-//             switch (operation) {
-//                 case 'plus':
-//                     result += `+` + `${num}`;
-//                     break;
-//                 case 'minus':
-//                     result += `-` + `${num}`;
-//                     break;
-//                 case 'into':
-//                     result += `*` + `${num}`;
-//                     break;
-//                 case 'divide':
-//                     if (num !== 0) {
-//                         result += `/` + `${num}`;
-//                     } else {
-//                         res.writeHead(400, { 'Content-Type': 'text/html' });
-//                         res.write('Division by zero is not allowed');
-//                         res.end();
-//                         return;
-//                     }
-//                     break;
-//                 default:
-//                     res.writeHead(400, { 'Content-Type': 'text/html' });
-//                     res.write('Invalid operation');
-//                     res.end();
-//                     return;
-//             }
-//         }
-
-//         const evaluate = eval(result);
-
-//         // Add the operation and result to the history
-//         history.unshift({ operation: result, result: evaluate });
-//         if (history.length > maxHistorySize) {
-//             history.pop();
-//         }
-
-//         res.writeHead(200, { 'Content-Type': 'text/html' });
-//         res.write(`Question: ${result}   Result: ${evaluate}`);
-//         res.end();
-//     } else {
-//         res.writeHead(400, { 'Content-Type': 'text/html' });
-//         res.write('Invalid URL format');
-//         res.end();
-//     }
-// });
-
-
-
-// process.on('exit', () => {
-//     try {
-//         fs.writeFileSync(historyFilePath, JSON.stringify(history), 'utf8');
-//     } catch (error) {
-//         console.error('Error writing history file:', error);
-//     }
-// });
-
-
-
-
-
-// server.listen(port, function(error) {
-//     if (error) {
-//         console.log('Something went wrong', error);
-//     } else {
-//         console.log('Server is listening on port ' + port);
-//     }
-// });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const http = require('http');
-// const url = require('url');
-
-// const port = 3000;
-// const maxHistorySize = 20;
-// const history = [];
-
-// const server = http.createServer(function(req, res) {
-//     const parsedUrl = url.parse(req.url, true);
-//     const pathSegments = parsedUrl.pathname.split('/');
-
-//     if (pathSegments[1] === 'history') {
-//         // Handle the /history endpoint
-//         res.writeHead(200, { 'Content-Type': 'text/html' });
-//         const historyList = history.map(entry => `Operation: ${entry.operation}, Result: ${entry.result}`).join('<br>');
-//         res.write(`Last 20 operations:<br>${historyList}`);
-//         res.end();
-//     } else if (pathSegments.length >= 4 && pathSegments.length % 2 === 0) {
-//         let result = "";
-//         result += parseFloat(pathSegments[1]);
-
-//         for (let i = 2; i < pathSegments.length; i += 2) {
-//             const operation = pathSegments[i].toLowerCase();
-//             const num = parseFloat(pathSegments[i + 1]);
-
-//             switch (operation) {
-//                 case 'plus':
-//                     result += `+` + `${num}`;
-//                     break;
-//                 case 'minus':
-//                     result += `-` + `${num}`;
-//                     break;
-//                 case 'into':
-//                     result += `*` + `${num}`;
-//                     break;
-//                 case 'divide':
-//                     if (num !== 0) {
-//                         result += `/` + `${num}`;
-//                     } else {
-//                         res.writeHead(400, { 'Content-Type': 'text/html' });
-//                         res.write('Division by zero is not allowed');
-//                         res.end();
-//                         return;
-//                     }
-//                     break;
-//                 default:
-//                     res.writeHead(400, { 'Content-Type': 'text/html' });
-//                     res.write('Invalid operation');
-//                     res.end();
-//                     return;
-//             }
-//         }
-
-//         const evaluate = eval(result);
-
-//         // Add the operation and result to the history
-//         history.unshift({ operation: result, result: evaluate });
-//         if (history.length > maxHistorySize) {
-//             history.pop();
-//         }
-
-//         res.writeHead(200, { 'Content-Type': 'text/html' });
-//         res.write(`Question: ${result}   Result: ${evaluate}`);
-//         res.end();
-//     } else {
-//         res.writeHead(400, { 'Content-Type': 'text/html' });
-//         res.write('Invalid URL format');
-//         res.end();
-//     }
-// });
-
-// server.listen(port, function(error) {
-//     if (error) {
-//         console.log('Something went wrong', error);
-//     } else {
-//         console.log('Server is listening on port ' + port);
-//     }
-// });
-
-
-
 
